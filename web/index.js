606445b0778a7f1671cd720bff733adee82a0deb
Adrià <adriasabate31@gmail.com>
2021-10-20 20:20:51 +0200

    web

---
+++ web/index.js
@@ -0,0 +1,11 @@
+function func() {
+      var text;
+      if (confirm("Ets major d'edat?")){
+            text="Confirmant...";
+       }
+       else {
+            text="Permís denegat";
+       }
+       document.getElementById("demo").innerHTML = text;
+}
+
